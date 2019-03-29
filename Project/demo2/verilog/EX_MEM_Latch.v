0dcf2c48a46070084532b695bdaf892a41fdaab3
kwilson33 <kwilson33@wisc.edu>
2019-03-29 16:39:28 -0500

    Added more stuff

--- Project/demo2/verilog/EX_MEM_Latch.v
+++ Project/demo2/verilog/EX_MEM_Latch.v
@@ -1,6 +1,23 @@
 module EX_MEM_Latch ();
 
-	input
-	output
+//TODO: Check if our branching and jump are connected to the correct signals
+//TODO: Figure out hasAB
+
+input clk, rst, en, RegWrite_in, DMemWrite_in, MemToReg_in, DMemDump_in, Branching_in, Jump_in; 
+input [15:0] aluOutput_in, B_in, updatedPC_in; 
+	
+output RegWrite_out, DMemWrite_out, MemToReg_out, DMemDump_out, Branching_out, Jump_out; 
+output [15:0] aluOutput_out, B_out, updatedPC_out; 
+
+register_16bits rf_EXMEM_aluOutput_out(.readData(aluOutput_out), .clk(clk), .rst(rst), .writeData(aluOutput_in), .writeEnable(en));
+register_16bits rf_EXMEM_B_out(.readData(B_out), .clk(clk), .rst(rst), .writeData(B_in), .writeEnable(en));
+register_16bits rf_EXMEM_updatedPC_out(.readData(updatedPC_out), .clk(clk), .rst(rst), .writeData(updatedPC_in), .writeEnable(en));
+
+dff dff_EXMEM_RegWrite_out(RegWrite_in, RegWrite_out, clk, rst);
+dff dff_EXMEM_DMemWrite_out(DMemWrite_in, DMemWrite_out, clk, rst);
+dff dff_EXMEM_MemToReg_out(MemToReg_in, MemToReg_out, clk, rst);
+dff dff_EXMEM_DMemDump_out(DMemDump_in, DMemDump_out, clk, rst);
+dff dff_EXMEM_Branching_out(Branching_in, Branching_out, clk, rst);
+dff dff_EXMEM_Jump_out(Jump_in, Jump_out, clk, rst);
 
 endmodule
--- Project/demo2/verilog/MEM_WB_Latch.v
+++ Project/demo2/verilog/MEM_WB_Latch.v
@@ -1,3 +1,20 @@
 module MEM_WB_Latch ();
 
+//TODO: Jump and Link (JAL enable)
+//TODO: is Branching_in what we want?
+//TODO: PC stuff?
+input clk, rst, en, Branching_in, RegWrite_in, MemToReg_in; 
+input [15:0] aluOutput_in, readData_in;
+
+output Branching_out, RegWrite_out, MemToReg_out; 
+output [15:0] aluOutput_out, readData_out;
+
+register_16bits rf_MEMWB_aluOutput_out(.readData(aluOutput_out), .clk(clk), .rst(rst), .writeData(aluOutput_in), .writeEnable(en));
+register_16bits rf_MEMWB_readData_out(.readData(readData_out), .clk(clk), .rst(rst), .writeData(readData_in), .writeEnable(en));
+
+dff dff_MEMWB_Branching_out(Branching_in, Branching_out, clk, rst);
+dff dff_MEMWB_RegWrite_out(RegWrite_in, RegWrite_out, clk, rst);
+dff dff_MEMWB_MemToReg_out(MemToReg_in, MemToReg_out, clk, rst);
+
+
 endmodule
