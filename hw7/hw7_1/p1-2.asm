28191d7fe35a61109db52b32607738003d921b50
mrobbins5 <mrobbins5@wisc.edu>
2019-03-29 12:45:15 -0500

    Finished HW6

--- hw7/hw7_1/p1-2.asm
+++ hw7/hw7_1/p1-2.asm
@@ -1,1 +1,9 @@
 // Write your assembly program for Problem 1 (a) #2 here.
+
+//Mem to EX forwarding
+lbi r0, 5
+lbi r1, 3
+add r2, r0, r1
+addi r1, r1, 1
+add r3, r3, r2
+halt
--- hw7/hw7_2/p2.asm
+++ hw7/hw7_2/p2.asm
@@ -1,1 +1,10 @@
 // Write your assembly program for Problem 2 (a) here.
+
+//Branch Prediction
+lbi r1, 0
+lbi r2, 1
+slt r0, r2, r1
+bnez r0, .test
+NOP
+.test:
+halt
