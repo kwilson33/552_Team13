f0a8dba4936c1202b1be4f22b1d9c95e0530787f
kwilson33 <kwilson33@wisc.edu>
2019-03-29 18:28:13 -0500

    Mark R

--- Project/demo2/verilog/Hazard_Detector.v
+++ Project/demo2/verilog/Hazard_Detector.v
@@ -1,4 +1,16 @@
 module Hazard_Detector ();
 
+input [15:0] instruction;
+input 
+
+output stall, pc_write_en, IF_ID_write_en; 
+
+//Stall conditions
+//Practice Midterm Solution --- JK don't use these it says not for general case!!!!
+//ED/EX.RegWrite = 1 and ID/EX.MemRead = 1 and IF/ID.Rs = ID/Ex.Rt
+
+//ID/EX.RegWrite = 0 and EX/MEM.RegWrite = 1 and EX/MEM.MemRead = 1 and IF/ID.Rs = EX/MEM.Rd.
+
+//EX/MEM.RegWrite = 1 and ID/EX.Rt
 
 endmodule
