8b50db8d5adb90aafbd9c745017658226a58abdf
Kevin Wilson <kwilson33@wisc.edu>
2019-03-29 15:27:42 -0500

    connected IF_ID_Latch in proc.v

--- Project/demo2/verilog/IF_ID_Latch.v
+++ Project/demo2/verilog/IF_ID_Latch.v
@@ -1,21 +1,28 @@
-module IF_ID_Latch ( instr_in, PC_In, en, clk, rst, instr_out, PC_Out);
+module IF_ID_Latch ( instruction_in, PC_In, en, clk, rst, IF_ID_instruction_out, PC_Out);
 
-//Input from instruction memory
-input [15:0] instr_in, PC_In; 
-input en, clk, rst; 
-output [15:0] instr_out, PC_Out; 
+  //Input from instruction memory
+  input [15:0] instruction_in, PC_In; 
+  input en, clk, rst; 
+  output [15:0] instruction_out, PC_Out; 
 
 
-register_16bits rfIFID(.readData(instr_out), .clk(clk), .rst(rst), .writeData(instr_in), .writeEnable(en));
-register_16bits rfIFID2(.readData(PC_Out), .clk(clk), .rst(rst), .writeData(PC_In), .writeEnable(en));
+  // if rst is asserted, set instruction to the NOP opcode 
+  wire [15:0] instruction_in_NOP_sel; 
+  assign instruction_in_NOP_sel = (rst) ? 16'b00001_xxxxxxxxxxx : instruction_in;
 
-//TODO, do something with reset, STALL?
+  // use 16 bit registers to decide if we should use current saved output or update output for
+  // the PC and the instruction
+  register_16bits rf_instruction(.readData(instr_out), .clk(clk), .rst(rst), 
+                                 .writeData(instruction_in_NOP_sel), 
+                                 .writeEnable(en));
+
+  register_16bits rf_PC(.readData(PC_Out), .clk(clk), 
+                        .rst(rst), .writeData(PC_In), 
+                        .writeEnable(en));
+
+  // TODO: Another output for PC
+  //register_16bits rf_PC_2(.out(anotherPCOut), .in(instrInSel), .en(en), .rst(1'b0), .clk(clk));
 
 endmodule 
 
-/*
-	wire [15:0] instrInSel; 
-	assign instrInSel = (rst) ? 16'b00001_xxxxxxxxxxx : instrIn;
-
-	dff16 mod3(.out(instrOut), .in(instrInSel), .en(en), .rst(1'b0), .clk(clk));
 */
--- Project/demo2/verilog/proc.v
+++ Project/demo2/verilog/proc.v
@@ -14,36 +14,46 @@ module proc (/*AUTOARG*/
    output err;
 
    // signals for the fetch, decode, memory, and execute
-   wire [15:0] updatedPC, next_PC_normal, instruction, 
+   wire [15:0] updatedPC, fetch_next_PC_normal, fetch_instruction_Out, 
 			   readData, writeData,
          // alu_B is the register we're storing into memory
          aluOutput, alu_A, alu_B;
+
+  // Added 3/29
+  wire [15:0] IF_ID_instruction_Out, IF_ID_WriteEnable, IF_ID_PC_Out;
         
    wire createDump, errDecode , JAL_en;
 
    
    /*
-   * This module instantiates the instruction memory and the PC Register to keep track of the current PC
+   * This module instantiates the fetch_instruction_Out memory and the PC Register to keep track of the current PC
    * there is also an adder instantiated here to increment the PC
   */  
   // ################################################### FETCH #######################################################
   fetchInstruction      instructionFetch(.clk(clk), .rst(rst), .PC_In(updatedPC), 
-									                       .dump(createDump), .PC_Next(next_PC_normal), 
-									                       .instruction(instruction));
+									                       .dump(createDump), .PC_Next(fetch_next_PC_normal), 
+									                       .fetch_instruction_Out(fetch_instruction_Out));
 
 
   // ################################################### IF_ID_Stage #######################################################
 
-   IF_ID_Latch          IF_ID_Stage ();
+   //TODO: Maybe need another PC output???
+   // Many of these signals probably not correct, including instruction_in. 3/29
+   IF_ID_Latch          IF_ID_Stage (.instruction_in(fetch_instruction_Out), 
+                                    .instruction_out(IF_ID_instruction_Out),
+                                    .en(IF_ID_WriteEnable), 
+                                    .clk(clk), .rst(rst),
+                                    .PC_In(fetch_next_PC_normal), .PC_Out(IF_ID_PC_Out));
+
 
   /*
    * This module instantiates the control unit and the register file. The control decides what to do 
-   * with the instruction and the register file is told what to do. The control unit also makes
+   * with the fetch_instruction_Out and the register file is told what to do. The control unit also makes
    * signals like SESelect which the regFile doesn't use.
    */
    // ################################################### DECODE #######################################################
   decodeInstruction     instructionDecode(.clk(clk), .rst(rst), .writeData(writeData), 
-									                        .instruction(instruction), .err(errDecode), .dump(createDump),
+									                        .fetch_instruction_Out(IF_ID_instruction_out), .err(errDecode), .dump(createDump),
                                           .A(alu_A), .B(alu_B), .Cin(Cin));
 
   // ################################################### DETECT HAZARDS #######################################################
@@ -60,7 +70,7 @@ module proc (/*AUTOARG*/
 
 
    // ################################################### EXECUTE #######################################################
-  executeInstruction    instructionExecute(.reg7_En(JAL_en), .instr(instruction), 
+  executeInstruction    instructionExecute(.reg7_En(JAL_en), .instr(fetch_instruction_Out), 
                                            .invA(instructionDecode.controlUnit.invA),
                                            .invB(instructionDecode.controlUnit.invB), 
                                            .Cin(Cin), 
@@ -68,7 +78,7 @@ module proc (/*AUTOARG*/
                                            .ALUSrc2(instructionDecode.controlUnit.ALUSrc2),
                                            .Branching(instructionDecode.controlUnit.Branching),
                                            .A(alu_A), .B(alu_B), 
-                                           .next_PC_normal(next_PC_normal), 
+                                           .fetch_next_PC_normal(fetch_next_PC_normal), 
                                            .aluOutput(aluOutput), .updatedPC(updatedPC));
 
 
@@ -97,7 +107,7 @@ module proc (/*AUTOARG*/
   // ################################################### WRITEBACK #######################################################
   writebackOutput       instructionWriteback(.readData(readData), .writeData(writeData), 
                                              .aluOutput(aluOutput),
-                                             .PC_Next(next_PC_normal), 
+                                             .PC_Next(fetch_next_PC_normal), 
                                              .memToReg(instructionDecode.controlUnit.MemToReg), 
                                              .JAL_en(JAL_en));
 						
